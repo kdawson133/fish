ae7860633154d61697a41357828eb3a18d9d4408
kdawson133 <kdawson133@gmail.com>
2023-01-16 16:10:20 +1000

    updated

--- config.fish
+++ config.fish
@@ -3,12 +3,23 @@ if status is-interactive
 end
 
 set fish_greeting ""
-set RBENV_ROOT ~/.config/rbenv
 set -gx TERM xterm-256color
+set -gx COLORTERM truecolor
 set -gx EDITOR nvim
+set -gx LC_ALL en_AU.UTF-8
 set -gx LANGUAGE en_AU:en
 set -gx LANG en_AU.UTF-8
-set -gx LC_ALL en_AU.UTF-8
+set -gx PYTHONSTARTUP /etc/python3/pythonrc
+set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
+set -gx PATH $CARGO_HOME/bin:$PATH
+set -gx RBENV_ROOT $HOME/.config/rbenv
+set -gx PATH $HOME/.config/rbenv/bin:$PATH
+set -gx PATH $HOME/.config/rbenv/shims:$PATH
+set -gx GEM_HOME $HOME/.config/gems
+set -gx PATH $HOME/.config/gems/bin:$PATH
+
+
 # theme
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
@@ -17,8 +28,10 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_display_user no
 set -g theme_hide_hostname no
 set -g theme_hostname always
+
 # aliases
 source (dirname (status --current-filename))/config-aliases.fish
+
 # OS specific
 switch (uname)
   case Darwin
@@ -28,5 +41,6 @@ switch (uname)
   case '*'
     source (dirname (status --current-filename))/config-windows.fish
 end
+
 # init rbenv
 status --is-interactive; and ~/.config/rbenv/bin/rbenv init - fish | source
