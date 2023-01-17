7bd26682606b2203c062d6592cabd600395ddaf1
kdawson133 <kdawson133@gmail.com>
2023-01-17 10:55:03 +1000

    updated

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ if status is-interactive
   cd  # Commands to run in interactive sessions can go here
 end
 
+# EXPORTS
 set fish_greeting ""
 set -gx TERM xterm-256color
 set -gx COLORTERM truecolor
@@ -12,14 +13,23 @@ set -gx LANG en_AU.UTF-8
 set -gx PYTHONSTARTUP /etc/python3/pythonrc
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
-set -gx PATH $CARGO_HOME/bin:$PATH
 set -gx RBENV_ROOT $HOME/.config/rbenv
-set -gx PATH $HOME/.config/rbenv/bin:$PATH
-set -gx PATH $HOME/.config/rbenv/shims:$PATH
 set -gx GEM_HOME $HOME/.config/gems
-set -gx PATH $HOME/.config/gems/bin:$PATH
-
-# theme
+# PATHS
+fish_add_path $CARGO_HOME/bin
+fish_add_path $RBENV_ROOT/bin
+fish_add_path $RBENV_ROOT/shims
+fish_add_path $GEM_HOME/bin
+if test -d $HOME/bin
+  fish_add_path $HOME/bin
+end
+if test -d $HOME/.local/bin
+  fish_add_path $HOME/.local/bin
+end
+if test -d /usr/local/bin
+  fish_add_path /usr/local/bin
+end
+# THEME
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme nord
@@ -28,10 +38,10 @@ set -g theme_display_user no
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-# aliases
+# ALIASES
 source (dirname (status --current-filename))/config-aliases.fish
 
-# OS specific
+# OS Specific
 switch (uname)
   case Darwin
     source (dirname (status --current-filename))/config-macos.fish
