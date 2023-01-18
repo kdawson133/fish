de62a191d8f33c3d3bb5b6e2ee9b8c9ed12b5a55
kdawson133 <kdawson133@gmail.com>
2023-01-18 10:49:01 +1000

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
 set -gx RBENV_ROOT $HOME/.config/rbenv
 set -gx GEM_HOME $HOME/.config/gems
+
 # PATHS
 fish_add_path $CARGO_HOME/bin
 fish_add_path $RBENV_ROOT/bin
@@ -29,6 +30,7 @@ end
 if test -d /usr/local/bin
   fish_add_path /usr/local/bin
 end
+
 # THEME
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
@@ -40,6 +42,9 @@ set -g theme_hostname always
 
 # ALIASES
 source (dirname (status --current-filename))/config-aliases.fish
+
+# ABBREVIATIONS
+source (dirname (status --current-filename))/config-abbr.fish
 
 # OS Specific
 switch (uname)
