364e20dda8ff58d931742fe70726212b64c601b9
kdawson133 <kdawson133@gmail.com>
2023-01-18 11:06:32 +1000

    updated

--- .config/fish/config-aliases.fish
+++ .config/fish/config-aliases.fish
@@ -1,6 +1,19 @@
-# reflagged aliases only
+#
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133@fosstodon.org
+#         
+# config-aliases.fish
+#
+# Reflagged Aliases Only
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
 alias fd='fd -IHiaL'
-
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,17 @@
+#
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133@fosstodon.org
+#         
+# config.fish
+#
 if status is-interactive
   cd  # Commands to run in interactive sessions can go here
 end
